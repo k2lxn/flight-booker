dcf08e31bc53d07fb8e79e9d5cf886e9f06ad03e
Jane <jane.e.reynolds@gmail.com>
2015-11-25 14:15:05 -0800

    Add Flight form at root

---
+++ app/controllers/flights_controller.rb
@@ -0,0 +1,14 @@
+class FlightsController < ApplicationController
+  def index
+  	@flight = Flight.new
+		
+		@airports = Airport.all.map {|a| [a.code, a.id]}
+		
+		# Get all flight dates
+		@flight_dates = []
+		Flight.all.each do |flight|
+			flight_date = flight.depart_time.strftime("%m/%d/%Y")
+			@flight_dates.push(flight_date) unless @flight_dates.include?(flight_date)
+		end
+  end
+end
